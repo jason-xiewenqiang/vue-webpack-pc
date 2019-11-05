14847c59857690e7f94e3cb34b9f1317bdaa3f71
xiewenqiang <2780535515@qq.com>
2019-11-05 16:22:34 +0800

    添加热更新

--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -11,6 +11,17 @@ const baseConfig = require('./webpack.base.conf')
 module.exports = merge(baseConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  //默认 false，也就是不不开启
+  watch: true,
+  //只有开启监听模式时，watchOptions才有意义
+  wathcOptions: {
+    //默认为空，不监听的文件或者文件夹，支持正则匹配
+    ignored: /node_modules/,
+    //监听到变化发生后会等300ms再去执行，默认300ms
+    aggregateTimeout: 300,
+    //判断文件是否发生变化是通过不停询问系统指定文件有没有变化实现的，默认每秒问1000次
+    poll: 1000
+  },
   devServer: {
     // contentBase: path.resolve(__dirname, '../dist'),
     host: 'localhost',
